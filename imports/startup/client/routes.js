23936ce4fa2b9cb5adf3fcc5b6444ebf5015aae0
Samuel <hackmew@gmail.com>
2021-03-02 18:13:20 +0100

    Add admin template & imports.

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,14 +3,22 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '../../ui/layouts/app_body.js';
 import '../../ui/layouts/storyEditor.js';
+import '../../ui/layouts/admin.js';
 
 
-// FlowRouter.route('/', {
-//   name: 'App.home',
-//   action() {
-//     BlazeLayout.render('App_body');
-//   },
-// });
+FlowRouter.route('/', {
+  name: 'App.home',
+  action() {
+    BlazeLayout.render('App_body');
+  },
+});
+
+FlowRouter.route('/admin', {
+  name: 'admin',
+  action() {
+    BlazeLayout.render('admin');
+  },
+});
 
 FlowRouter.route('/editor/:environment', {
 	name: 'storyEditor',
---
+++ imports/ui/layouts/admin.js
@@ -0,0 +1,8 @@
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+import { StoryDev } from '../../api/story/story.js';
+import { StoryProd } from '../../api/story/story.js';
+
+import './admin.html';
+import './admin.css';
