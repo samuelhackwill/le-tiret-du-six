98070323fe1c181fa727aec69bcf88c876751a6a
Samuel <hackmew@gmail.com>
2021-11-10 16:55:19 +0100

    Add server side function to update words DB with ressource status

--- imports/api/words/server/publications.js
+++ imports/api/words/server/publications.js
@@ -15,6 +15,12 @@ Meteor.methods({
 
 
 	letterHarvestCall(_env, _letterId){
+		_params = _letterId.match(/([A-zÀ-ÿ]+)\W([A-zÀ-ÿ])/)
+		_word = _params[1]
+		_letter = _params[2]
+
+		Words.update({env:_env, "data.name":_word}, {$push : {"data.$.harvestedLetters" : _letter} })
+
 		sendMessage({action:"killLetter", letterId:_letterId, env:_env})
 	}
 })
